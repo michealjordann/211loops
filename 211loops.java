dddb434e1100c8766368ba5b9d776533a2e23fd5
Tanush P <148394954+michealjordann@users.noreply.github.com>
2025-11-17 14:27:30 -0800

    Create 211loops.java

---
+++ 211loops.java
@@ -0,0 +1,20 @@
+/*
+ * Activity 2.1.1
+ */
+import java.util.Scanner;
+
+public class Divisibility
+{
+  public static void main(String[] args)
+  {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter the dividend: ");
+    int dividend = sc.nextInt();
+    System.out.println("Enter the divisor: ");
+    int divisor = sc.nextInt();
+
+    
+      
+  }
+}
