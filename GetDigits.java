c1ae57c9370c3809f94fe65fa108835d2f59a5a1
Tanush P <148394954+michealjordann@users.noreply.github.com>
2025-11-18 18:06:50 -0800

    Add GetDigits class to display digits of an integer

---
+++ GetDigits.java
@@ -0,0 +1,21 @@
+/*
+ * Activity 2.1.1
+ */
+import java.util.Scanner;
+
+public class GetDigits
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive integer to see its digits: ");
+        int num = sc.nextInt();
+        while (num > 0) {
+            int digit = num % 10;
+            System.out.println(digit);
+            num = num / 10;
+
+
+
+        }
+}
